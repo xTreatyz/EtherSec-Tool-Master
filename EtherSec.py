413245b0599c380a33b4704414dd4cdc0acf3889
xTreatyz <42012327+xTreatyz@users.noreply.github.com>
2018-08-11 15:17:29 -0400

    Add files via upload

---
+++ EtherSec.py
@@ -0,0 +1,34 @@
+import subprocess
+print "----------------------------------------- Welcome to the EtherSec-Windows-Tool -----------------------------------------"
+print "               ..."
+print "             ;::::;                 ~NSP/xTreatyz          "
+print "           ;::::; :;"
+print "         ;:::::'   :;"
+print "        ;:::::;     ;."
+print "       ,:::::'       ;           OOOO"
+print "       ::::::;       ;          OOOOOO"
+print "       ;:::::;       ;         OOOOOOOO"
+print "      ,;::::::;     ;'         / OOOOOOO"
+print "    ;:::::::::`. ,,,;.        /  / DOOOOOO"
+print "  .';:::::::::::::::::;,     /  /     DOOOO"
+print " ,::::::;::::::;;;;::::;,   /  /        DOOO"
+print ";`::::::`'::::::;;;::::: ,#/  /          DOOO"
+print ":`:::::::`;::::::;;::: ;::#  /            DOOO"
+print "::`:::::::`;:::::::: ;::::# /              DOO"
+print "`:`:::::::`;:::::: ;::::::#/               DOO"
+print " :::`:::::::`;; ;:::::::::##                OO"
+print " ::::`:::::::`;::::::::;:::#                OO"
+print " `:::::`::::::::::::;'`:;::#                O"
+print "  `:::::`::::::::;' /  / `:#"
+print "   ::::::`:::::;'  /  /   `#"
+print "\r\n"
+print "Choose your options: (1-2)"
+print "[1] ScreenLogger Builder"
+print "[2] LoginServer Builder"
+print "\r\n"
+Option = raw_input('Option: ')
+print "\r\n"
+if Option == '1':
+    subprocess.call("ScreenLogger.py", shell=True)
+if Option == '2':
+    subprocess.call("LoginServer.py", shell=True)
